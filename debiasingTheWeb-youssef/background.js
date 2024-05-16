725361626e36c235167196b9b32f428c2e6a1c10
YoussefZairi <49310741+YoussefZairi@users.noreply.github.com>
2024-05-16 14:50:36 +0100

    Update background.js

--- debiasingTheWeb-youssef/background.js
+++ debiasingTheWeb-youssef/background.js
@@ -1,19 +1,18 @@
-// Listen for tab updates
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // Check if the URL has changed and the tab is active
-  if (changeInfo.url && tab.active) {
+  // Check if the URL has changed, the tab is active, and the URL is a valid website
+  if (changeInfo.url && tab.active && (tab.url.startsWith('http://') || tab.url.startsWith('https://'))) {
     console.log('Tab URL changed:', tab.url);
 
     // Send a fetch request to the backend with the domain of the current tab
     const url = new URL(tab.url);
-    const domain = url.hostname;
-
+    console.log(url);
+    
     fetch('http://localhost:3000/api', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ domain }),
+      body: JSON.stringify({ url }),
     })
       .then((response) => response.json())
       .then((data) => {
